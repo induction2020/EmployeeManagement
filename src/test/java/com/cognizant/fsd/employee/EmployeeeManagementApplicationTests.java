67b678ecc8a3632f07698d228654d5438cedfbff
induction2020 <thiruppathi.me@gmail.com>
2019-04-13 17:22:17 +0530

    Thiru2

--- src/test/java/com/cognizant/fsd/employee/EmployeeeManagementApplicationTests.java
+++ src/test/java/com/cognizant/fsd/employee/EmployeeeManagementApplicationTests.java
@@ -1,16 +1,31 @@
 package com.cognizant.fsd.employee;
 
+import static org.junit.Assert.assertEquals;
+
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(
+	    properties = "spring.datasource.type=com.zaxxer.hikari.HikariDataSource"
+		)
 public class EmployeeeManagementApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
+	
+	@Autowired
+    private DataSource dataSource;
+ 
+    @Test
+    public void hikariConnectionPoolIsConfigured() {
+        assertEquals("com.zaxxer.hikari.HikariDataSource", dataSource.getClass().getName());
+    }
 
 }
