be05389d37816e025af483ede8b31131090e3e22
induction2020 <thiruppathi.me@gmail.com>
2019-04-13 16:58:07 +0530

    Thiru

---
+++ src/main/java/com/cognizant/fsd/employee/EmployeeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.fsd.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeeManagementApplication.class, args);
+	}
+
+}
